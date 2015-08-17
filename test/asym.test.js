17e54fc91d4731191699582f3fb9f386aa3231ef
Nelson <contact.nelsonic@gmail.com>
2015-08-17 21:20:13 +0100

    remove redundant test from asym. everything in test/env.test.js now!

--- test/asym.test.js
+++
@@ -1,9 +0,0 @@
-/**
- * This test is *designed* to test the scenario where a person does NOT
- * specify where their config.env file is located and they are calling
- * env2 as a module they installed fron npm and required from node_modules
- *
- */
- // setTimeout(function(){
-  //  require('./node_modules/parent.test.js'); // executes file inside "fake" node_modules
- // },2000)
